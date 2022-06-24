246f00a3dfacc33ddeca8e0e365d451f31aa62ce
DinoHerlentz <87923606+DinoHerlentz@users.noreply.github.com>
2022-06-24 16:09:10 +0700

    Add files via upload

---
+++ spirograph.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+bgcolor("black")
+pensize(2)
+speed(0)
+
+for i in range(6):
+    for colors in ['red', 'magenta', 'blue', 'cyan', 'green', 'yellow', 'white']:
+        color(colors)
+        circle(100)
+        left(10)
+
+hideturtle()
